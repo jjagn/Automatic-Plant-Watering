75aa99611ef8d86dd75c96a65603f859469e0a4a
jjagn <72364545+jjagn@users.noreply.github.com>
2022-01-19 22:44:55 +1300

    initial commit automatic watering project

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#define DEBUG
+
+#ifdef DEBUG
+
+#endif
+
+void setup() {
+  // put your setup code here, to run once:
+
+  const int input = A0;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
